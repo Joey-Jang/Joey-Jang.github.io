f6edb26106288f6fa6825528a03658f5fbdce158
Joey-Jang <joey.ngn.jang@gmail.com>
2022-01-21 23:44:58 +0900

    fix home page

--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -1,8 +1,24 @@
-$(document).ready(function() {
-    $(".post").on("click", ".post-category a", function(event) {
-        if($(event.delegateTarget).attr("route") === window.location.hash.substring(1)) {
-            location.reload();
-            $(window).scrollTop(0);
-        };
+// $(document).ready(function() {
+//     $(".post").on("click", ".post-category a", function(event) {
+//         if($(event.delegateTarget).attr("route") === window.location.hash.substring(1)) {
+//             location.reload();
+//             $(window).scrollTop(0);
+//         };
+//     });
+// });
+
+//
+$(function() {
+    $(window).on("resize", function() {
+        console.log("window resized");
+
+        if(NexT.utils.isMobile()) {
+            console.log("is mobile")
+
+            const spanWrapper = $(".span-wrapper");
+            const period = spanWrapper.children("span").eq(1);
+            $(".span-wrapper").children("span").eq(1).detach();
+            spanWrapper.parent().append(period);
+        }
     });
-});
+})
